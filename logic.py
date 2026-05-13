547c48eaa89b3cf43fde91e158d67a9bb68b6c90
MrSponge123123123 <liamberner@web.de>
2026-05-13 14:33:56 +0200

    organized calculation logic

--- logic.py
+++ logic.py
@@ -29,6 +29,82 @@ def get_right_num_as_str(term:str, sign_index: int) -> list:
 
 
 def calculate_as_str(term: str) -> str:
+    def remove_unnecessary_float(num: str) -> str:
+        if "." in num:
+            while num[-1] == "0":
+                num = num.removesuffix("0")
+
+            if num[-1] == ".":
+                num = num.removesuffix(".")
+
+        return num
+    def solve_multiplication(term: str) -> str:
+        while "*" in term:
+            mul_index: int = term.find("*")
+            num_1_str, num_1_start_index = get_left_num_as_str(term, mul_index)
+            num_2_str, num_2_end_index = get_right_num_as_str(term, mul_index)
+
+            # pars to float
+            num_1 = float(num_1_str)
+            num_2 = float(num_2_str)
+
+            # get result and replace in term
+            result_str = str(num_1 * num_2)
+            result_str = remove_unnecessary_float(result_str)
+
+            term = term[:num_1_start_index] + result_str + term[num_2_end_index + 1:]
+
+        return term
+    def solve_division(term: str) -> str:
+        while "/" in term:
+            div_index: int = term.find("/")
+            num_1_str, num_1_start_index = get_left_num_as_str(term, div_index)
+            num_2_str, num_2_end_index = get_right_num_as_str(term, div_index)
+
+            # parse to float
+            num_1 = float(num_1_str)
+            num_2 = float(num_2_str)
+
+            # get result and replace in term
+            result_str = str(num_1 / num_2)
+            result_str = remove_unnecessary_float(result_str)
+            term = term[:num_1_start_index] + result_str + term[num_2_end_index + 1:]
+
+        return term
+    def solve_addition(term: str) -> str:
+        while "+" in term:
+            add_index: int = term.find("+")
+            num_1_str, num_1_start_index = get_left_num_as_str(term, add_index)
+            num_2_str, num_2_end_index = get_right_num_as_str(term, add_index)
+
+            # parse to float
+            num_1 = float(num_1_str)
+            num_2 = float(num_2_str)
+
+            # get result and replace in term
+            result_str = str(num_1 + num_2)
+            result_str = remove_unnecessary_float(result_str)
+            term = term[:num_1_start_index] + result_str + term[num_2_end_index + 1:]
+
+        return term
+    def solve_subtraction(term: str) -> str:
+        while "-" in term:
+            sub_index: int = term.find("-")
+            num_1_str, num_1_start_index = get_left_num_as_str(term, sub_index)
+            num_2_str, num_2_end_index = get_right_num_as_str(term, sub_index)
+
+            # parse to float
+            num_1 = float(num_1_str)
+            num_2 = float(num_2_str)
+
+            # get result and replace in term
+            result_str = str(num_1 - num_2)
+            result_str = remove_unnecessary_float(result_str)
+
+            term = term[:num_1_start_index] + result_str + term[num_2_end_index + 1:]
+
+        return term
+
     # remove all spaces
     term = term.replace(" ", "")
 
@@ -43,93 +119,9 @@ def calculate_as_str(term: str) -> str:
         term = term[:index_start] + result + term[index_end + 1:]
 
 
-    while any(op in term for op in ["*", "/", "+", "-"]):
-        # check for multiplication
-        if "*" in term:
-            mul_index: int = term.find("*")
-            num_1_str, num_1_start_index = get_left_num_as_str(term, mul_index)
-            num_2_str, num_2_end_index = get_right_num_as_str(term, mul_index)
-
-            # pars to float
-            num_1 = float(num_1_str)
-            num_2 = float(num_2_str)
-
-            # get result and replace in term
-            result_str = str(num_1 * num_2)
-            if "." in result_str:
-                while result_str[-1] == "0":
-                    result_str = result_str.removesuffix("0")
-
-                if result_str[-1] == ".":
-                    result_str = result_str.removesuffix(".")
-
-
-            term = f"{term[:num_1_start_index]}{result_str}{term[num_2_end_index + 1:]}"
-
-
-        # check for division
-        if "/" in term:
-            div_index: int = term.find("/")
-            num_1_str, num_1_start_index = get_left_num_as_str(term, div_index)
-            num_2_str, num_2_end_index = get_right_num_as_str(term, div_index)
-
-            # parse to float
-            num_1 = float(num_1_str)
-            num_2 = float(num_2_str)
-
-            # get result and replace in term
-            result_str = str(num_1 / num_2)
-            if "." in result_str:
-                while result_str[-1] == "0":
-                    result_str = result_str.removesuffix("0")
-
-                if result_str[-1] == ".":
-                    result_str = result_str.removesuffix(".")
-
-            term = f"{term[:num_1_start_index]}{result_str}{term[num_2_end_index + 1:]}"
-
-
-        # check for addition
-        if "+" in term:
-            add_index: int = term.find("+")
-            num_1_str, num_1_start_index = get_left_num_as_str(term, add_index)
-            num_2_str, num_2_end_index = get_right_num_as_str(term, add_index)
-
-            # parse to float
-            num_1 = float(num_1_str)
-            num_2 = float(num_2_str)
-
-            # get result and replace in term
-            result_str = str(num_1 + num_2)
-            if "." in result_str:
-                while result_str[-1] == "0":
-                    result_str = result_str.removesuffix("0")
-
-                if result_str[-1] == ".":
-                    result_str = result_str.removesuffix(".")
-
-            term = f"{term[:num_1_start_index]}{result_str}{term[num_2_end_index + 1:]}"
-
-
-        # check for subtraction
-        if "-" in term:
-            sub_index: int = term.find("-")
-            num_1_str, num_1_start_index = get_left_num_as_str(term, sub_index)
-            num_2_str, num_2_end_index = get_right_num_as_str(term, sub_index)
-
-            # parse to float
-            num_1 = float(num_1_str)
-            num_2 = float(num_2_str)
-
-            # get result and replace in term
-            result_str = str(num_1 - num_2)
-            if "." in result_str:
-                while result_str[-1] == "0":
-                    result_str = result_str.removesuffix("0")
-
-                if result_str[-1] == ".":
-                    result_str = result_str.removesuffix(".")
-
-            term = f"{term[:num_1_start_index]}{result_str}{term[num_2_end_index + 1:]}"
+    term = solve_multiplication(term)
+    term = solve_division(term)
+    term = solve_addition(term)
+    term = solve_subtraction(term)
 
     return term
